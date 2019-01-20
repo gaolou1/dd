242f613d22f4d32868e8690f7ea417137ff9df83
gaolou1 <gaolou1573@163.com>
2019-01-20 17:43:16 +0800

    edit on branch hot_fix

--- src/main/java/d/dd.java
+++ src/main/java/d/dd.java
@@ -3,6 +3,7 @@ package d;
 public class dd {
 public static void main(String[] args) {
 	System.out.println("ddddd");
-	System.out.println();
+	System.out.println("sdasdsa");
+	System.out.println("hot fix ....");
 }
 }
